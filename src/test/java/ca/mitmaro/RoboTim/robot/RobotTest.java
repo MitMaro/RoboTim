7b6337c72905d1786eef6955585da149082c1160
Tim Oram <mitmaro@gmail.com>
2012-12-10 01:01:01 -0330

    Fixed robot package tests from not running and fixed broken robot tests

--- src/test/java/ca/mitmaro/RoboTim/robot/RobotTest.java
+++ src/test/java/ca/mitmaro/RoboTim/robot/RobotTest.java
@@ -15,7 +15,7 @@ import ca.mitmaro.RoboTim.robot.responder.exception.InvalidResponse;
 public class RobotTest {
 
 	@Test
-	public void handle() throws InvalidResponse {
+	public void handle() throws InvalidResponse, InvalidCommandException {
 		
 		Connection c = Mockito.mock(Connection.class);
 		ResponderHandler rh = Mockito.mock(ResponderHandler.class);
@@ -24,23 +24,26 @@ public class RobotTest {
 		Robot r = new Robot(c);
 		
 		r.registerResponder(rh);
+		
+		r.handle(cmd);
 		
 		Mockito.verify(rh).handle(r, cmd);
 	}
 	
 	@Test(expectedExceptions = {InvalidCommandException.class})
-	public void handle_invalid() throws InvalidResponse {
+	public void handle_invalid() throws InvalidCommandException, InvalidResponse {
 		Connection c = Mockito.mock(Connection.class);
 		ResponderHandler rh = Mockito.mock(ResponderHandler.class);
 		ca.mitmaro.RoboTim.irc.command.Command cmd
 			= Mockito.mock(ca.mitmaro.RoboTim.irc.command.Command.class);
 		Robot r = new Robot(c);
 		
-		Mockito.doThrow(new InvalidCommandException("message")).when(rh).handle(r, cmd);
+		Mockito.doThrow(new InvalidResponse("message")).when(rh).handle(r, cmd);
 		
 		r.registerResponder(rh);
 		
-		rh.handle(r, cmd);
+		r.handle(cmd);
+
 	}
 
 	@Test
@@ -50,7 +53,7 @@ public class RobotTest {
 		
 		r.sendMessage("message");
 		
-		Mockito.verify(c).sendMessage("message");
+		Mockito.verify(c).sendMessage("message\r\n");
 	}
 
 	@Test
@@ -60,9 +63,9 @@ public class RobotTest {
 		
 		r.sendMessage(new String[] {"message1", "message2", "message3"});
 
-		Mockito.verify(c).sendMessage("message1");
-		Mockito.verify(c).sendMessage("message2");
-		Mockito.verify(c).sendMessage("message3");
+		Mockito.verify(c).sendMessage("message1\r\n");
+		Mockito.verify(c).sendMessage("message2\r\n");
+		Mockito.verify(c).sendMessage("message3\r\n");
 	}
 
 	@Test
@@ -70,7 +73,7 @@ public class RobotTest {
 		Connection c = Mockito.mock(Connection.class);
 		Robot r = new Robot(c);
 		Command cmd = Mockito.mock(Command.class);
-		r.registerStartupCommand(cmd);
+		r.registerShutdownCommand(cmd);
 		
 		r.shutdown();
 		
